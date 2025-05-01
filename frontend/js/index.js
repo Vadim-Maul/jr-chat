0b5688ccfc3a67c78c677cad8d7a3e38f8a45e3f
Vadim-Maul <v.v.maul681@gmail.com>
2025-05-01 17:35:56 +0200

    Enhance message control button styling and update message rendering structure

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,38 +1,43 @@
 {
-  const container = document.querySelector(".messages");
+	const container = document.querySelector('.messages');
 
-  function renderMessages(messages) {
-    for (const message of messages) {
-      const messageElement = document.createElement("article");
-      messageElement.className = "message";
-
-      messageElement.innerHTML = `
+	function renderMessages(messages) {
+		for (const message of messages) {
+			const messageElement = document.createElement('article');
+			messageElement.className = 'message';
+			const hours = new Date(message.timestamp).getHours();
+			const minutes = new Date(message.timestamp).getMinutes();
+			messageElement.innerHTML = `
         <div class="message-author">${message.username}</div>
-        <button class="message-control"></button>
+        <button class="message-control">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
         <p class="message-text">${message.text}</p>
-        <time>${message.timestamp}</time>
+        <time>${hours}:${minutes}</time>
       `;
 
-      container.appendChild(messageElement);
-    }
-  }
+			container.appendChild(messageElement);
+		}
+	}
 
-  function initChat() {
-    fetch("http://localhost:4000/messages", {
-      method: "GET",
-    })
-      .then(function (messagesResponse) {
-        if (messagesResponse.status !== 200) {
-          throw new Error("Couldn't get messages from server");
-        }
+	function initChat() {
+		fetch('http://localhost:4000/messages', {
+			method: 'GET',
+		})
+			.then(function (messagesResponse) {
+				if (messagesResponse.status !== 200) {
+					throw new Error("Couldn't get messages from server");
+				}
 
-        return messagesResponse.json();
-      })
-      .then(function (messagesList) {
-        console.log(messagesList);
-        renderMessages(messagesList);
-      });
-  }
+				return messagesResponse.json();
+			})
+			.then(function (messagesList) {
+				console.log(messagesList);
+				renderMessages(messagesList);
+			});
+	}
 
-  initChat();
+	initChat();
 }
